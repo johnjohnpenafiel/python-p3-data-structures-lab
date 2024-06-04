f33112823b10d5f3fea642360bc989029a87e2c4
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-06-04 16:59:55 -0400

    Data Structures Lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,43 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    food_names = []
+    for item in spicy_foods:
+        food_names.append(item['name'])
+    return food_names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciests_foods = []
+    for item in spicy_foods:
+        if item['heat_level'] > 5:
+            spiciests_foods.append(item)
+    return spiciests_foods
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for item in spicy_foods:
+        print(f"{item['name']} ({item['cuisine']}) | Heat Level: {item['heat_level']* '🌶'}")
+        
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    cuisine_dict = []
+    for item in spicy_foods:
+        if item['cuisine'] == cuisine:
+            return item
+    
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for item in spicy_foods:
+        if item['heat_level'] > 5:
+            print(f"{item['name']} ({item['cuisine']}) | Heat Level: {item['heat_level']* '🌶'}")
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    average = 0
+    for item in spicy_foods:
+        average += item['heat_level']
+    return average/3
 
+        
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+   spicy_foods.append(spicy_food)
+   return spicy_foods
